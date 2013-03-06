902e57bc14dee667fda143584ff618c15d0bcad8
Sam Whitehall <sgw1g10@ecs.soton.ac.uk>
2013-03-06 16:55:33 +0000

    k-nearest neighbours implemented for picking word for colour value; tests updated.

--- knowledge/colour_test.py
+++ knowledge/colour_test.py
@@ -9,8 +9,32 @@ class TestAddingExpressions(unittest.TestCase):
     def testLearnSingle(self):
         red_expr = Expression(["COLOUR", "r_255", "g_0", "b_10"])
         self.knowledge.learn("red", red_expr)
-        self.assertIn(("red", (255, 0, 10)), self.knowledge.points)
-        pass
+        self.assertIn((255, 0, 10), self.knowledge.points)
+        self.assertIn("red", self.knowledge.words)
+
+    def testKNearest(self):
+        black_expr = Expression(["COLOUR", "r_0", "g_0", "b_0"])
+        dark_red = Expression(["COLOUR", "r_125", "g_0", "b_0"])
+        reddish_expr = Expression(["COLOUR", "r_250", "g_25", "b_30"])
+
+        self.knowledge.learn("black", black_expr)
+        self.knowledge.learn("red", dark_red)
+        self.knowledge.learn("red", reddish_expr)
+
+        self.knowledge.word_for(Expression(["COLOUR", "r_255", "g_0", "b_0"]))
+        self.assertTrue(False)
+
+
+    def testNoColourLabel(self):
+        black_expr = Expression(["COLOUR", "r_0", "g_0", "b_0"])
+        dark_red = Expression(["COLOUR", "r_125", "g_0", "b_0"])
+        reddish_expr = Expression(["COLOUR", "r_250", "g_25", "b_30"])
+
+        self.knowledge.learn("black", black_expr)
+        self.knowledge.learn("red", dark_red)
+        self.knowledge.learn("red", reddish_expr)
+
+        self.assertEqual(self.knowledge.expression_for("blue"), Expression("MISUNDERSTOOD"))
 
 if __name__ == '__main__':
     unittest.main()
--- knowledge/knn_colour.py
+++ knowledge/knn_colour.py
@@ -1,3 +1,7 @@
+from itertools import izip
+from collections import Counter
+from training.expression import Expression
+
 class KNNColourSemantics:
     """
     This class stores the "knowledge" the speaker-learner has about
@@ -11,25 +15,49 @@ class KNNColourSemantics:
     def __init__(self, k=3):
        self.k = k
        self.points = []
+       self.words = []
        pass
+
+    def _unpack_expression(self, expression):
+        """Abstract away unpacking of numerical (R,G,B) tuple from expressions of the
+        form <COLOUR r_X g_Y b_Z>"""
+        return tuple( int(subexpr.subexpressions[0].name[2:])
+                      for subexpr in expression.subexpressions[1:] )
 
     def learn(self, word, expression):
         """Add new entry into mapping (as it's been learned by a word-
         learning algorithm in the framework)."""
 
-        # expression is of the form <COLOUR r_X g_Y b_Z>
-        vals = []
-        for subexpression in expression.subexpressions[1:]:
-            vals.append(int(subexpression.subexpressions[0].name[2:]))
-
-        self.points.append((word, tuple(vals)))
+        # maintain two (ordered) lists of points & colour words
+        self.points.append(self._unpack_expression(expression))
+        self.words.append(word)
 
     def word_for(self, expression):
         """Query to get a word for a certain colour value (point). This may
         not in general be one-to-one, -- there may be multiple words
-        for a single colour value, so a random word will be selected."""
-        pass
+        for a single colour value, so a random word will be selected. Although
+        k-nearest neighbour will produce a unique result."""
+
+        given = self._unpack_expression(expression)
+
+        # create tuple of sum of squared errors for each known colour point
+        errs = tuple(sum((p - x)**2 for p,x in izip(point, given)) for point in self.points)
+
+        # errors sorted low->high and the point indicies from errs used here (i.e. argsort)
+        min_errs_args = sorted(range(len(errs)), key=lambda k: errs[k])
+
+        # return colour label with most votes (modal average from k nearest
+        k_nearest_colours = tuple(self.words[point] for point in min_errs_args[:self.k])
+        colour_counts = Counter(k_nearest_colours)
+        most_common = max(name for name, count in colour_counts.items())
+
+        return most_common
 
     def expression_for(self, word):
         """Return a typical colour value (point) belonging to a colour label"""
-        pass
+
+        # first check colour label exists, otherwise return MISUNDERSTOOD expression
+        if word not in self.words:
+            return Expression("MISUNDERSTOOD")
+        else:
+            pass
