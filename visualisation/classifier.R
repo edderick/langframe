038f7897cbeeab2da085e743aa22766c607370eb
Sam Whitehall <me@samwhitehall.com>
2013-03-13 19:20:43 +0000

    kknn package didn't seem to work; implemented basic knn from scratch, passes sanity check for results

--- visualisation/classifier.R
+++ visualisation/classifier.R
@@ -1,8 +1,6 @@
 #!/usr/bin/RScript
 
 
-library(kknn)
-
 args <- commandArgs(TRUE)
 chosen.lang.name <- args[1]
 k.nearest <- args[2]
@@ -10,22 +8,32 @@ k.nearest <- args[2]
 all.data <- read.delim("stdin", sep=",", stringsAsFactors=TRUE, header=TRUE,
                         na.strings="")
 
-# partition into training & test sets
+# partition into training & test sets (training has ".def" at end)
 data.train <- all.data[all.data$lang.name == paste(chosen.lang.name,".def",sep=""),]
 data.test <- all.data[all.data$lang.name == chosen.lang.name,]
 
-data.train[c("x","y","z")]
-data.test[c("x","y","z")]
+# KNN classification algorithm for a single element of the test set
+knn.classify <- function(element) {
+    train <- data.train
 
-as.factor(data.train$word)~.
+    elem.x <- as.numeric(element[['x']])
+    elem.y <- as.numeric(element[['y']])
+    elem.z <- as.numeric(element[['z']])
 
-result <- kknn(formula=as.factor(data.train$word)~., 
-                train=data.train[c("x","y","z")],
-                test=data.test[c("x","y","z")], 
-                k=1,
-                distance=2, #minkowski distance 2 = euclidean
-                kernel="rectangular")
-data.test$word <- result$fitted.values
+    square.errors <- (train$x - elem.x)^2 + (train$y - elem.y)^2 + (train$z - elem.z)^2
+    k.nearest.indices <- order(square.errors, decreasing=FALSE)[1:k.nearest]
+    k.nearest.words <- data.train$word[k.nearest.indices]
+
+    unique.words <- unique(k.nearest.words)
+    mode.avg.word <- unique.words[which.max(tabulate(match(k.nearest.words, unique.words)))]
+
+    return (mode.avg.word)
+}
+
+# perform KNN classification on new data points to get colour labels
+new.colours <- apply(data.test, 1, knn.classify)
+data.test$word <- new.colours
 
 new.data <- rbind(data.train, data.test)
-new.data
+
+write.table(new.data, quote=FALSE, sep=",", row.names=FALSE)
