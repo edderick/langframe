79d89a1f8157cb7c09be2bac0282e470deab3f26
Sam Whitehall <sgw1g10@ecs.soton.ac.uk>
2013-03-22 20:16:41 +0000

    train_ab takes parameters from command line (not intefering with fileinput)

--- simulations/train_ab.py
+++ simulations/train_ab.py
@@ -6,28 +6,52 @@ Learner B is trained by utterances from Learner A. At each stage, output X with 
 words for X as LangBi (where i is iteration number).
 
 The distance between L(A) and L_B(i) should decrease steadily.
+
+INPUT: piped to stdin or filename as first argument
 """
 
 import fileinput
 import random
+import argparse
 
 from training.expression import Expression
 from knowledge import knn_colour
 
 def get_colour_expression(rgb):
+    """ convert to proper expression format to be interpretec"""
     return Expression(["COLOUR", "r_%d" % rgb[0],
                                  "g_%d" % rgb[1],
                                  "b_%d" % rgb[2] ])
 
 def probe_colour(lang_name, learner, rgb):
+    """print relevant entry in appropriate CSV format to stdout"""
     word = learner.word_for(get_colour_expression(rgb))
     print "%s,%s" % (lang_name, word)
 
-learnerA = knn_colour.KNNColourSemantics("A", k=3)
-learnerB = knn_colour.KNNColourSemantics("B", k=3)
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description="test training from one agent to another")
+
+parser.add_argument('files', nargs='?', help='specify input files o/w stdin')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-I', '--iterations', dest="training_iterations",
+                    type=int, nargs="?", default=100)
+group.add_argument('-N', '--samples', dest="num_samples",
+                    type=int, nargs="?", default=1000)
+group.add_argument('--skip', dest="skip",
+                    type=int, nargs="?", default=1)
+group.add_argument('--k', dest="knearest",
+                    type=int, nargs="?", default=3)
+
+args = parser.parse_args()
+
+# set up learner
+learnerA = knn_colour.KNNColourSemantics("A", k=args.knearest)
+learnerB = knn_colour.KNNColourSemantics("B", k=args.knearest)
 
 # train A from stdin
-for line in fileinput.input():
+for line in fileinput.input(args.files):
     entry = line.split(",")
 
     if entry[0] == "lang.name":
@@ -40,16 +64,16 @@ for line in fileinput.input():
 # header on stdout
 print "lang.name,word"
 
-# pick 1000 random colours
+# pick sample of N random colours
 test_colours = [(random.randint(0,255), random.randint(0,255), random.randint(0,255))
-                    for i in range(100)]
+                    for i in range(args.num_samples)]
 
 # output for learner A
 for rgb_tuple in test_colours:
     probe_colour("langA", learnerA, rgb_tuple)
 
-# iteratively train learner B on A's utterances
-for i in range(100):
+# iteratively train learner B on A's utterances (N iterations)
+for i in range(0, args.training_iterations, args.skip):
     (word, meaning) = learnerA.say_something()
     learnerB.learn(word, meaning)
 
