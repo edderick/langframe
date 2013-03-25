5979e5e860a5d68890ad2501c37e9782dfb8ca6c
Sam Whitehall <me@samwhitehall.com>
2013-03-25 21:37:59 +0000

    args changed; this should be changed to avoid file synchronisation issues

--- visualisation/argsAB.py
+++ visualisation/argsAB.py
@@ -3,7 +3,7 @@
 # short & sweet script to generate the args needed for
 # language_distance.R for AB training
 
-iterations=100
+iterations=25
 skip=1
 
 print " ".join(["langA,langB_%d" % i for i in range(0,iterations,skip)])
