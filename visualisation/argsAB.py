d8f7b173cbe94801910045fe0b0fafb54b87361d
Sam Whitehall <me@samwhitehall.com>
2013-03-25 17:46:28 +0000

    small script to expand out appropriate command line arguments for language_distance.R

---
+++ visualisation/argsAB.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# short & sweet script to generate the args needed for
+# language_distance.R for AB training
+
+iterations=100
+skip=1
+
+print " ".join(["langA,langB_%d" % i for i in range(0,iterations,skip)])
