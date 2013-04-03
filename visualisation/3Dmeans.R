892bf6f4356d955b9f3c1474887a9a6fdac6b2ad
Sam Whitehall <me@samwhitehall.com>
2013-04-03 23:24:29 +0100

    adds cross symbol for created words

--- visualisation/3Dmeans.R
+++ visualisation/3Dmeans.R
@@ -8,18 +8,19 @@ args <- commandArgs(TRUE)
 all.data <- read.delim("stdin", sep=",", stringsAsFactors=TRUE, header=TRUE,
                         na.strings="")
 
-levels(all.data$lang.name)
-
 for(lang in levels(all.data$lang.name)) {
     relevant.rows <- subset(all.data, all.data$lang.name == lang)
+
+    print(relevant.rows)
 
     attach(relevant.rows)
     my.cols <- c("black", "darkblue", "green", "red", "cyan", "yellow", "magenta", "gray")
     axis.lim <- c(1, 255)
+    my.points <- c(20,20,20,20,20,20,20,20,4)
 
     graph.name <- paste("means for ", lang, sep="")
 
-    graph <- cloud(b~r*g, main=graph.name, col=my.cols, pch=20, cex=2.5, shade=TRUE,
+    graph <- cloud(r~g*b, main=graph.name, col=my.cols, pch=my.points, cex=2.5, shade=TRUE,
         xlim=axis.lim, ylim=axis.lim, zlim=axis.lim)
     print(graph)
 }
